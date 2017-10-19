24e038c4b82bc45ccd4f34694bf1c3c6758df20d
lfrFlores <luis_naiby_flores@outlook.com>
2017-10-19 11:39:45 -0500

    Lista de Ciudades

--- src/beans/CiudadService.java
+++ src/beans/CiudadService.java
@@ -8,16 +8,16 @@ List<Ciudad> listadoCiudades= new ArrayList<Ciudad>();
 public List<Ciudad> ciudades(){
 	
 	//1° Forma
-	listadoCiudades.add(new Ciudad(1,"Lima",200));
-	listadoCiudades.add(new Ciudad(2,"Cajamarca",3));
-	listadoCiudades.add(new Ciudad(3,"Trujillo",4));
-	listadoCiudades.add(new Ciudad(4,"Piura",1));
+	listadoCiudades.add(new Ciudad(1,"Lima",20000));
+	listadoCiudades.add(new Ciudad(2,"Cajamarca",30000));
+	listadoCiudades.add(new Ciudad(3,"Trujillo",40000));
+	listadoCiudades.add(new Ciudad(4,"Piura",100000));
 	
 	//2° Forma
 	Ciudad c5= new Ciudad();
 	c5.setId(5);
 	c5.setNombre("Moquegua");
-	c5.setPoblacion(20);
+	c5.setPoblacion(20000);
 	
 	listadoCiudades.add(c5);
 	
